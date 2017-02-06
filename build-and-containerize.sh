7a09929ef7bbe804c25f62d0abe5d7974505b871
ragnaradolf <ragnar.adolf@gmail.com>
2017-02-06 22:10:07 +0000

    Basic docker configuration added with nginx

---
+++ build-and-containerize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo 
+echo "Building react app"
+npm run build
+
+echo 
+echo "Moving build folder to docker directory"
+cp -R ./build ./docker/server
+cd docker
+
+echo
+echo "Removing raaserver docker container if present"
+docker rm -f raaserver
+
+echo 
+echo "Building and starting raaserver docker container"
+docker-compose up -d
