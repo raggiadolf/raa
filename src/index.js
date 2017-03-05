0c90f23911f66f8dfea95dace308c9a41a697446
ragnaradolf <ragnar.adolf@gmail.com>
2017-03-05 21:23:38 +0000

    Adding react-router and trying out google analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import App from './Components/App';
 import './Styles/index.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-93092203-1');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 ReactDOM.render(
-  <App />,
+  <Router history={ hashHistory } onUpdate={ logPageView }>
+    <Route path="*" component={ App } />
+  </Router>,
   document.getElementById('root')
 );
